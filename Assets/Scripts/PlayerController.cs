93d45091228a63433c689f9d662035079d49a022
lin62 <asterlin7890@gmail.com>
2025-11-11 13:01:43 -0500

    Add week 10 package

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public enum FacingDirection
+    {
+        left, right
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
+        //manage the actual movement of the character.
+        Vector2 playerInput = new Vector2();
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerInput = Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerInput = Vector2.left;
+        }
+        MovementUpdate(playerInput);
+    }
+
+    private void MovementUpdate(Vector2 playerInput)
+    {
+        rb.linearVelocity = playerInput * speed;
+    }
+
+    public bool IsWalking()
+    {
+        return false;
+    }
+    public bool IsGrounded()
+    {
+        return true;
+    }
+
+    public FacingDirection GetFacingDirection()
+    {
+        return FacingDirection.left;
+    }
+}
--- Assets/Scripts/TorqueTest.cs
+++ Assets/Scripts/TorqueTest.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public class TorqueTest : MonoBehaviour
 {
-    public Rigidbody2D rigidbody;
+    public Rigidbody2D rb;
 
     void Start()
     {
-        //rigidbody.AddTorque(2, ForceMode2D.Impulse);
+        rb.AddTorque(2, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
         float turn = Input.GetAxis("Horizontal");
-        rigidbody.AddTorque(-turn * 10f);
+        rb.AddTorque(-turn * 10f);
     }
 }
