f0ecca21e833cb50815a64f12a5c5e021a01ee5a
lin62 <asterlin7890@gmail.com>
2025-11-13 12:02:02 -0500

    Finish the player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
+
+    public float rayCastOffset;
     public enum FacingDirection
     {
         left, right
@@ -24,33 +26,49 @@ public class PlayerController : MonoBehaviour
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && IsGrounded())
         {
             playerInput = Vector2.right;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && IsGrounded())
         {
             playerInput = Vector2.left;
         }
         MovementUpdate(playerInput);
+
+        //if (IsGrounded() == false)
+        //{
+        //    Debug.Log("not grounded");
+        //}
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-        rb.linearVelocity = playerInput * speed;
+        rb.linearVelocityX = playerInput.x * speed;
     }
 
     public bool IsWalking()
     {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            return true;
+        }
         return false;
     }
     public bool IsGrounded()
     {
-        return true;
+        return Physics2D.Raycast(transform.position, Vector2.down, rayCastOffset);
     }
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            return FacingDirection.right;
+        }
+        else
+        {
+            return FacingDirection.left;
+        }
     }
 }
